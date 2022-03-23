06ad46acf14e44dc92d6a5a27caec3e68e154950
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-03-23 10:01:05 +0100

    add new modifications

---
+++ src/components/footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="centent">
+    <footer>
+      <p>
+        Author: example<br />
+        <a href="example@example.com">example@example.com</a>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterContent",
+};
+</script>
+
+<style>
+footer {
+  text-align: center;
+  padding: 3px;
+}
+</style>
